102781efb90cdad6768fc3833c59657177b3fb94
JasmineHu2025 <JasmineHu2025>
2025-04-08 16:53:06 +0800

    0408-1

---
+++ src/App-0408-1.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1>Hi,React</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusamus suscipit praesentium, sequi laborum ex molestias illo veritatis neque placeat ad voluptatum tempore consectetur minus doloribus quibusdam libero unde necessitatibus.</p>
+        </>
+    )
+}
+export default App;
