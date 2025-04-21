63479f1e397339833776c90f23484d875f1f66ae
JasmineHu2025 <JasmineHu2025>
2025-04-21 10:49:54 +0800

    0421-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './0416/App-0416-1'
+import App from './0421/App-0421-1.jsx'
 // import './index.css'
-import'./style.css'
+import './style.css'
+import { BrowserRouter } from 'react-router-dom'  /* 終端機先停止後輸入npm i react-router-dom 接者<BrowserRouter>打完會自己出現 */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
