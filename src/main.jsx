a87a3e4eba63b2e20aa1087ba008165ca62e0aa8
JasmineHu2025 <JasmineHu2025>
2025-05-12 09:26:10 +0800

    0512-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './0429/App-faq.jsx'
+import App from './0512/App.jsx'
 // import './index.css'
 // import './style.css'
 import { BrowserRouter } from 'react-router-dom'  /* 終端機先停止後輸入npm i react-router-dom 接者<BrowserRouter>打完會自己出現 */
